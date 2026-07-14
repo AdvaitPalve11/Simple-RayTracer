b5b5d9ae5f0078b2a768eca21d96bc4c7f29a82f
Advait <advaitdpalve@gmail.com>
2026-07-14 18:50:28 +0530

    Test Renderer

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,11 @@
 #include <iostream>
+#include "renderer.h"
 #include "Vec3.h"
 #include "ray.h"
 int main()
 {
-    Vec3 a;
-    Vec3 b(1, 2, 3);
-    Vec3 c = a+b;
-    Vec3 d = a-b;
-    Vec3 x (1,0,0);
-    Vec3 y (0,1,0);
-   
-    Vec3 z = Vec3::cross(x,y);
-
-    a.print();
-    b.print();
-    c.print();
-    d.print();
-
-   
-    z.print();
-
-    std::cout<<d.length();
+   Renderer renderer (800 , 600);
+   renderer.render();
 
    
 
