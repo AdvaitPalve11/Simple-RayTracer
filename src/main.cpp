a09b2235060693b4844e340ea189b72d3f758b74
Advait <advaitdpalve@gmail.com>
2026-07-14 14:03:56 +0530

    Test All vector Operations

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,21 @@
 int main()
 {
     Vec3 a;
-
     Vec3 b(1, 2, 3);
+    Vec3 c = a+b;
+    Vec3 d = a-b;
+    Vec3 adotb = Vec3::cross(a,b);
+    Vec3 axb = Vec3::cross(a,b);
 
-    std::cout << a.x << " "
-              << a.y << " "
-              << a.z << '\n';
+    a.print();
+    b.print();
+    c.print();
+    d.print();
 
-    std::cout << b.x << " "
-              << b.y << " "
-              << b.z << '\n';
+    adotb.print();
+    axb.print();
+
+    std::cout<<d.length();
 
     return 0;
 }
