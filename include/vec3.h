7670b5ec69276d05563d32645d63f8b655e71b57
Advait <advaitdpalve@gmail.com>
2026-07-14 13:56:31 +0530

    Declare  vector operations

--- include/vec3.h
+++ include/vec3.h
@@ -8,10 +8,23 @@ class Vec3
         double y;
         double z;
 
-    Vec3() : x(0), y(0) , z(0){}; // Initializer list
-       
+    Vec3() : x(0), y(0) , z(0){}; // Initializer list   
     Vec3(double x , double y , double z) : x(x), y(y),z(z){}; 
         
+    //Operations
+    Vec3 operator+(const Vec3& other) const;
+    Vec3 operator-(const Vec3& other) const;
+    Vec3 operator*(double scalar)const;
+    Vec3 operator/(double scalar)const;
     
 
+    double length() const;
+    double lengthSquared() const;
+    Vec3 normalized() const;
+    
+    static double dot(const Vec3& a, const Vec3& b);
+
+
+    //misc
+    void print() const;
 };
