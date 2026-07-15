3f245b88d1dc4f656d8b1a2d54f45beafb6c4f61
Advait <advaitdpalve@gmail.com>
2026-07-15 15:48:20 +0530

    Create a basic camera class

--- include/camera.h
+++ include/camera.h
@@ -1,11 +1,31 @@
 #pragma once
 
 #include "ray.h"
+#include "vec3.h"
 
 class Camera{
+
+    private:
+        Vec3 position;
+
+        Vec3 forward;
+        Vec3 up;
+        Vec3 right;
+
+        double fov;
+        double aspectRatio;
 
     public:
         Camera();
 
     Ray getRay(double u , double v) const;
+
+    void moveForward(double distance);
+    void moveRight(double distance);
+    void moveUp(double distance);
+
+    void rotateYaw(double angle);
+    void rotatePitch(double angle);
+
+    Vec3 getPosition() const;
 };
