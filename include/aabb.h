ad3b3c7847944a26157ff4dffd542bbb61b38a2d
Advait <advaitdpalve@gmail.com>
2026-08-06 22:37:29 +0530

    Add BVH Header and CPP files

---
+++ include/aabb.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "vec3.h"
+#include "ray.h"
+
+class AABB
+{
+private:
+    Vec3 minimum;
+    Vec3 maximum;
+
+public:
+    AABB();
+    AABB(const Vec3& minimum, const Vec3& maximum);
+
+    bool hit(const Ray& ray, double tMin, double tMax) const;
+
+    const Vec3& getMin() const;
+    const Vec3& getMax() const;
+};
---
+++ src/aabb.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Advait on 06-08-2026.
+//
+
+#include "aabb.h"
